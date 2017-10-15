75c4d9ce2495c354200a0b8792dd2fb82df2440b
Eric Chu <eric.chu.56@gmail.com>
2017-10-15 16:37:25 -0400

    Extract certain features from pretrained VGG16
    Functionality:
        1) Run 'vgg16.state_dict().keys()' in python console to see feature names, e.g. 'features.0', 'classifier.3'
        2) extractor = VGG16Extractor(vgg16, ['features.0', 'classifier.3'])
    
    Solution and reference: https://discuss.pytorch.org/t/how-to-extract-features-of-an-image-from-a-trained-model/119/8
        - The main solution is to just traverse model and see if name of module matches name you want to extract
        - But this only works on Sequential models
        - VGG16 actually has two Sequentials -- 1) features, 2) classifier
        - So in my forward(x), I have two for loops, one for features and other for classifier
        - Each time I do x = module(x)
        - However, it seems like there's some automatic reshaping between Sequentials.
            - vgg.features(x) returns a (1, 512, 7, 7)
            - vgg.classifier starts with a linear layer
            - simply doing vgg.classifier(vgg.features(x)) throws size mismatch error
            - So I explicit add a x.view(batch, -1) in between the two for loops
        - Another possible solution would be to register_forward_hooks, but I don't think that addreses my issue,
        which is traversing the model (nn graph).
        - Robust solution for any model would be to recursive check if module is a leaf module
            - i.e. if len(module._modules) == 0: x = module(x)
            - This worked actually, but then I had the problem of reshaping. Which is probably unrelated
    
    Test plan:
        - Python console
        - python painting_embeddings.py
            - Runs test_extract_VGG16_activations()
    
    Some other notes:
    ---
    Notes on a couple different properties and methods of nn.Module:
    vgg: printing this shows the structure
    vgg.state_dict().keys() gives you all modules, nested (e.g. features.0.weight)
        - Note state_dict gives you the weights, not modules
        - These are the same as the names of the modules (e.g. name is 'features.0')
        (which you could as get by iterating over model.named_modules(), which is a generator)
    vgg._modules.keys() (flat version, e.g. 'features' at first level)
    vgg.named_modules: generator for all named modules (tuple of (name, module))
    
    Remember <python_obj>.__dir__() gives you all attributes and methods
    
    Using the Selective saves memory, which is why I assume theyre not being saved in the first place

--- painting_embeddings.py
+++ painting_embeddings.py
@@ -10,6 +10,7 @@ Available functions:
 - test_artist_title_extraction: Test that artist and title are extracted correctly
 - test_lazy_load: Test lazy load of image and image properties functionality
 - test_one_painting: Create embedding for one painting
+- test_extract_VGG16_activations: Pass image through VGG16 and extract selected features
 
 """
 
@@ -17,15 +18,20 @@ from config import WIKIART_TEST_IMG_PATH
 import numpy as np
 import os
 from PIL import Image
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
 import torchvision.models as models
+from torchvision import transforms
 
 class Painting(object):
     """Represents a painting stored as an image.
 
     Public attributes:
     - path: path to image (str)
-    - artist: artist name extracted from path
-    - title: title of painting extracted from path
+    - artist: artist name extracted from path (str)
+    - title: title of painting extracted from path (str)
     
     - img_mode: mode of Pillow Image (e.g. RBG, L). Denotes number of channels
     - img_size: size of Pillow Image (e.g. (750, 326))
@@ -82,22 +88,90 @@ class Painting(object):
         valid = correct_num_channels and img_large_enough
         return valid
 
+    def get_img_variable_for_vgg16(self):
+        """Return pytorch Variable with correct dimensions as input into vgg16"""
+        transform = transforms.Compose([
+            transforms.ToPILImage(),            # should save PILImage upon original loading or refactor somehow
+            transforms.Scale(256),
+            transforms.RandomCrop(224),
+            transforms.ToTensor()])
+        tensor = transform(self.img)            # CWH format
+        tensor.unsqueeze_(0)                    # BCWH
+
+        return Variable(tensor)
 
     ####################################################################################################################
     # Representation
     ####################################################################################################################
     def create_embedding(self, vgg16):
-        content_emb = self._create_content_embedding(vgg16)
-        style_emb = self._create_style_embedding(vgg16)
+
+        vgg16 = VGG16Extractor(vgg16)
+        x = self.get_img_variable_for_vgg16()
+        print vgg16(x)
+
+        content_emb = 2
+        style_emb = 2
+
+        # content_emb = self._create_content_embedding(vgg16)
+        # style_emb = self._create_style_embedding(vgg16)
         embedding = [content_emb, style_emb]    # TODO: concat
         return embedding
+    #
+    # def _create_style_embedding(self, vgg16):
+    #     self.img
+    #     return 2
+    #
+    # def _create_content_embedding(self, vgg16):
+    #     return 3
 
-    def _create_style_embedding(self, vgg16):
-        self.img
-        return 2
 
-    def _create_content_embedding(self, vgg16):
-        return 3
+########################################################################################################################
+# Helper to extract activations from pre-trained model
+########################################################################################################################
+class VGG16Extractor(nn.Module):
+    """
+    VGG16 has two Sequential containers: 'features', and 'classifier'. Traverse all modules, saving activations
+    for the features we want to extract
+    
+    Public attributes:
+    - pretrained: VGG16 model
+    - features_names: list of names of features, e.g. ['features.4', 'classifier.3']
+    """
+    def __init__(self, pretrained, feature_names):
+        super(VGG16Extractor, self).__init__()
+
+        self.pretrained = pretrained
+        self.feature_names = feature_names
+
+    def forward(self, x):
+        """
+        Input:
+        - x: (batch, channels, x, y)
+        """
+        features = []
+
+        batch_size = x.size(0)
+
+        # Pass input through features part of model
+        for name, module in self.pretrained.features.named_modules():
+            if len(module._modules) == 0:       # first module is the entire thing, check for leaf nodes
+                full_name = 'features.{}'.format(name)
+                x = module(x)
+                if full_name in self.feature_names:
+                    features.append(x)
+
+        # Reshape for linear layer
+        x = x.view(batch_size, -1)
+
+        # Pass through classifier part of model (linear layers)
+        for name, module in self.pretrained.classifier.named_modules():
+            if len(module._modules) == 0:  # first module is the entire thing, check for leaf nodes
+                full_name = 'classifier.{}'.format(name)
+                x = module(x)
+                if full_name in self.feature_names:
+                    features.append(x)
+
+        return features
 
 ########################################################################################################################
 # Basic test cases
@@ -122,11 +196,22 @@ def test_one_painting_emb():
 
     print emb
 
+def test_extract_VGG16_activations():
+    """Pass image through VGG16 and extract selected features"""
+    vgg16 = Painting.get_vgg16()
+    vgg16_extractor = VGG16Extractor(vgg16, ['features.8'])
+    painting = Painting(WIKIART_TEST_IMG_PATH)
+    x = painting.get_img_variable_for_vgg16()
+    features = vgg16_extractor(x)
+    print features
+    print len(features)
 
 if __name__ == '__main__':
-    test_artist_title_extraction()
-    test_lazy_load()
-    test_one_painting_emb()
+    # test_artist_title_extraction()
+    # test_lazy_load()
+    # test_one_painting_emb()
+    test_extract_VGG16_activations()
+
 
     #     if torch.cuda.is_available():
     #         model = model.cuda()
